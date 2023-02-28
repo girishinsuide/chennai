9f800d6261bc64fe5625bf624dcd7d4452edde58
heena <heena.q@insuide.com>
2023-02-28 18:14:29 +0530

    dutyfree new

--- src/views/attheairport/DutyFree.vue
+++ src/views/attheairport/DutyFree.vue
@@ -30,7 +30,7 @@
    <p class="ma-2">Breitling: Watches For World<br> Class Travellers</p>
 
    <v-card-actions>
-                        <v-btn rounded dark class="gradient-button " style="padding:10px 30px !important">
+                        <v-btn rounded dark class="gradient-button text-capitalize" style="padding:10px 20px !important">
                            More Info
                         </v-btn>
                     </v-card-actions>
@@ -59,7 +59,7 @@
    <p>Upto 25% off Guerlain<br> Fragrances</p>
 
      <v-card-actions>
-          <v-btn rounded dark class="gradient-button " style="padding:10px 30px !important">
+          <v-btn rounded dark class="gradient-button text-capitalize ml-15" style="padding:10px 20px !important">
                            More Info
            </v-btn>
      </v-card-actions>
@@ -88,7 +88,7 @@
    <p>Explore the Nike tradition <br>with upto 15% off</p>
 
     <v-card-actions>
-          <v-btn rounded dark class="gradient-button " style="padding:10px 30px !important">
+          <v-btn rounded dark class="gradient-button text-capitalize ml-5" style="padding:10px 20px !important">
                            More Info
            </v-btn>
      </v-card-actions>
@@ -106,21 +106,11 @@
  <v-row no-gutters class="category mt-15">
       <v-col cols="2">
          <v-sheet class="pa-2 ">
-          <v-card
-          class="categoryBox px-1 bg-secondary rounded-xl"
-         
-         > 
+          <v-card > 
 
     <v-img
    
-      src="../../views/attheairport/images/fashion.png"
-       class="gradi-box"
-       height="200px"
-       width="230px"
-      
-         
-     
-    ></v-img>
+      src="../../views/attheairport/images/fashionbox.png"></v-img>
 
      <div class="imageBox-txt-two">
    <p class="ma-2">Fashion</p>
@@ -135,19 +125,11 @@
 
        <v-col cols="2">
             <v-sheet class="pa-2">
-          <v-card
-          class="categoryBox px-1 bg-secondary rounded-xl"
-         
-         > 
+          <v-card> 
 
     <v-img
    
-      src="../../views/attheairport/images/beauty.png"
-       class="gradi-box"
-        height="200px"
-          width="216px"
-     
-    ></v-img>
+      src="../../views/attheairport/images/beautybox.png"></v-img>
 
      <div class="imageBox-txt-two">
    <p class="ma-2">Beauty</p>
@@ -162,19 +144,11 @@
 
        <v-col cols="2">
             <v-sheet class="pa-2">
-          <v-card
-          class="categoryBox px-1 bg-secondary rounded-xl"
-        
-         > 
+          <v-card> 
 
     <v-img
    
-       src="../../views/attheairport/images/liquor.png"
-       class="gradi-box"
-       height="200px"
-       width="216px"
-     
-    ></v-img>
+       src="../../views/attheairport/images/liquorbox.png"></v-img>
 
      <div class="imageBox-txt-two">
    <p class="ma-2">Liquor</p>
@@ -189,19 +163,10 @@
 
        <v-col cols="2">
             <v-sheet class="pa-2">
-          <v-card
-          class="categoryBox px-1 bg-secondary rounded-xl"
-        
-         > 
+          <v-card> 
 
     <v-img
-   
-       src="../../views/attheairport/images/confectionary.png"
-       class="gradi-box"
-        height="200px"
-        width="216px"
-     
-    ></v-img>
+      src="../../views/attheairport/images/confectionarybox.png"></v-img>
 
      <div class="imageBox-txt-two">
    <p class="ma-2">Confectionary</p>
@@ -216,25 +181,16 @@
 
        <v-col cols="2">
             <v-sheet class="pa-2">
-          <v-card
-          class="categoryBox px-1 bg-secondary rounded-xl"
-         
-         > 
+          <v-card> 
 
     <v-img
    
-       src="../../views/attheairport/images/electronics.png"
-       class="gradi-box"
-        height="200px"
-        width="216px"
-     
-    ></v-img>
+       src="../../views/attheairport/images/electronicsbox.png"></v-img>
 
      <div class="imageBox-txt-two">
-   <p class="ma-2">Electronics</p>
+     <p class="ma-2">Electronics</p>
 
 
-   
       </div>
         </v-card>
         </v-sheet>
@@ -248,23 +204,24 @@
 
 
       
-        <v-row  class="mt-15" >
+  <v-row class="bestseller">
    <v-col cols="1" md="1">
      <v-img
-      width="40px"
+      width="60px"
+      height="60px"
       src="../../views/attheairport/images/bestsellerIcon.svg"
-      class="ml-5 mt-1" ></v-img></v-col>
+       class="mt-2"></v-img></v-col>
 
       <v-col cols="9" md="9">
-      <h2 class="darkgrey--text">Bestsellers </h2>
-      <p  class="grey--text">Our range of bestselling products available for your buying needs.</p>
+      <h2 class="mt-2">Bestsellers </h2>
+      <p>Our range of bestselling products available for your buying needs.</p>
       
        </v-col>
 
        <v-col cols="2" md="2">
     
          <v-card-actions>
-                        <v-btn rounded dark class="gradient-button " style="padding:10px 30px !important">
+                        <v-btn rounded dark class="gradient-button text-capitalize" style="padding:10px 30px !important">
                            View All
                         </v-btn>
                     </v-card-actions> </v-col>
@@ -274,164 +231,40 @@
 <v-row>
  
   <v-col cols="12" md="12">  
-    <v-sheet
-    class="mx-auto mt-2"
-    max-width="100%"
-   
-   >
-    <v-slide-group
-      v-model="BestsellersSlider"
-      show-arrows="false"
-      
-      
-    >
-      <v-slide-item
-        v-for="n in 15"
-        :key="n"
-        v-slot="{ toggle }"
-       
-        
-      >
-        <v-card
-          
-          class="ma-2 BestsellersSlider mt-5"
-          height="350"
-          width="330"
-          @click="toggle"
-          depressed 
-          flat
-         > 
-
-      <!-- <v-img :src="slideitem.src"></v-img> -->
-      <div class="BestsellersSliderImgBox px-1 bg-secondary rounded-xl">
-       <v-img
-       src="../../views/attheairport/images/bestslider1.png"
-      class=""
-    ></v-img>
-      </div>
-
-     <div class="BestsellersSlider-txt mt-7">
-       
-    <v-row>
- 
-   <v-col cols="8" md="8"> 
-      <p>Bvlgari Aqva Pour Homme </p>
-         <span>Marine EDP 100ML</span>
-         
-   </v-col>
-
-  <v-col cols="4" md="4">
-    <div  class="amtBox">₹ 7,250</div> 
     
-    </v-col>
-  
-  </v-row>
-   
-      </div>
-        </v-card>
-
-        
-      </v-slide-item>
-    </v-slide-group>
-  </v-sheet></v-col>
- 
-  </v-row>
-
-  
+  <dutyfreeBestsellerslider />  
     
+    
+    </v-col></v-row>
+
   
- 
 
 
 <!-- Bestsellers Slider End -->
 
 
-<v-row>
- 
-  <v-col cols="12" md="12">  
-    
-     <v-carousel :show-arrows="false"> 
-        <template v-for="(item, index) in slider"> 
-          <v-carousel-item  v-if="(index + 1) % columns === 1 || columns === 1" 
-                           :key="index"
-          > 
-            <v-row class="flex-nowrap" style="height:216px"> 
-              <template v-for="(n,i) in columns"> 
-                <template v-if="(+index + i) < slider.length"> 
-                  <v-col :key="i"> 
-                    <v-sheet v-if="(+index + i) < slider.length" 
-                             :color="slider[+index + i]"
-                             height="216px"
-                    >
-                      <v-row class="fill-height"
-                             align="center"
-                             justify="center"
-                      >
-                        <div class="display-3">
-    <v-img
-       src="../../views/attheairport/images/bestslider1.png"
-      class=""
-    ></v-img>
 
-                        </div>
-
-                    
-                      </v-row>
-
-                      <v-row class="fill-height"
-                             align="left"
-                           
-                      >
-                        <div class="BestsellersSlider-txt ml-5" style="border:1px solid">
-       
-    <v-row>
- 
-   <v-col cols="8" md="8"> 
-      <p>Bvlgari Aqva Pour Homme </p>
-         <span>Marine EDP 100ML</span>
-         
-   </v-col>
-
-  <v-col cols="4" md="4">
-    <div  class="amtBox">₹ 7,250</div> 
-    
-    </v-col>
-  
-  </v-row>
-   
-      </div>
-                      </v-row>
-                    </v-sheet>
-                  </v-col> 
-                </template> 
-              </template> 
-            </v-row> 
-          </v-carousel-item> 
-        </template> 
-      </v-carousel>  
-    
-    
-    </v-col></v-row>
 
 
 <!-- New Arrival Slider Start -->
-        <v-row>
+        <v-row class="newarrival">
    <v-col cols="1" md="1">
      <v-img
-      width="40px"
+      width="60px"
+      height="60px"
       src="../../views/attheairport/images/new-arrivals.svg"
-      class="ml-5 mt-1" ></v-img></v-col>
+       class="mt-2"></v-img></v-col>
 
       <v-col cols="9" md="9">
-      <h2 class="darkgrey--text">New Arrivals </h2>
-      <p  class="grey--text">For those looking for our latest releases, you’re in the right place.</p>
+      <h2 class="mt-2">New Arrivals </h2>
+      <p>For those looking for our latest releases, you’re in the right place.</p>
       
        </v-col>
 
        <v-col cols="2" md="2">
     
          <v-card-actions>
-                        <v-btn rounded dark class="gradient-button " style="padding:10px 30px !important">
+                        <v-btn rounded dark class="gradient-button text-capitalize" style="padding:10px 30px !important">
                            View All
                         </v-btn>
                     </v-card-actions> </v-col>
@@ -441,83 +274,30 @@
 <v-row>
  
   <v-col cols="12" md="12">  
-    <v-sheet
-    class="mx-auto mt-2"
-    max-width="100%"
+
+    <dutyfreeNewarrival />
    
-   >
-    <v-slide-group
-      v-model="NewArrivalSlider"
-      show-arrows="false"
-      hide-delimiter-background
-      
-      
-    >
-      <v-slide-item
-        v-for="n in 15"
-        :key="n"
-        v-slot="{ toggle }"
-       
-        
-      >
-        <v-card
-          
-          class="ma-2 NewArrivalSlider mt-5"
-          height="350"
-          width="330"
-          @click="toggle"
-          depressed 
-          flat
-         > 
-
-      <!-- <v-img :src="slideitem.src"></v-img> -->
-      <div class="NewArrivalSliderImgBox px-1 bg-secondary rounded-xl">
-       <v-img
-       src="../../views/attheairport/images/bestslider1.png"
-      class=""
-    ></v-img>
-      </div>
-
-     <div class="NewArrivalSlider-txt mt-7">
-       
-    <v-row>
- 
-   <v-col cols="8" md="8"> 
-      <p>Bvlgari Aqva Pour Homme </p>
-         <span>Marine EDP 100ML</span>
-         
    </v-col>
-
-  <v-col cols="4" md="4">
-    <div  class="amtBox">₹ 7,250</div> 
-    
-    </v-col>
-  
-  </v-row>
-   
-      </div>
-        </v-card>
-
-        
-      </v-slide-item>
-    </v-slide-group>
-  </v-sheet></v-col>
  
   </v-row>
 
 
   <!-- New Arrival Slider End -->
-<v-row class="mt-10 mb-15">
+
+  <!-- exclusive start -->
+
+<v-row class="mb-15 featuresBlk">
    <v-col cols="3" md="3">
     <v-row>
    <v-col cols="3" md="3">
      <v-img
-      width="40px"
-      src="../../views/attheairport/images/exclusive.svg" class="ml-2 mt-1"></v-img></v-col>
+      width="50px"
+      height="50px"
+      src="../../views/attheairport/images/exclusive.svg"></v-img></v-col>
 
       <v-col cols="9" md="9">
-      <h2 class="darkgrey--text">Exclusive </h2>
-      <p  class="grey--text">Website only Offers</p>
+      <h2>Exclusive </h2>
+      <p>Website only Offers</p>
       
        </v-col>
 
@@ -530,12 +310,13 @@
     <v-row>
    <v-col cols="3" md="3">
      <v-img
-      width="40px"
-      src="../../views/attheairport/images/savemore.svg" class="ml-2 mt-1"></v-img></v-col>
+       width="50px"
+      height="50px"
+      src="../../views/attheairport/images/savemore.svg"></v-img></v-col>
 
       <v-col cols="9" md="9">
-      <h2 class="darkgrey--text">Save more</h2>
-      <p  class="grey--text">Upto 70% off products</p>
+      <h2>Save more</h2>
+      <p>Upto 70% off products</p>
       
        </v-col>
 
@@ -548,12 +329,13 @@
     <v-row>
    <v-col cols="3" md="3">
      <v-img
-      width="40px"
-      src="../../views/attheairport/images/savemore.svg" class="ml-2 mt-1"></v-img></v-col>
+      width="50px"
+      height="50px"
+      src="../../views/attheairport/images/savemore.svg"></v-img></v-col>
 
       <v-col cols="9" md="9">
-      <h2 class="darkgrey--text">Browse and Buy </h2>
-      <p  class="grey--text">Convenience first</p>
+      <h2>Browse and Buy </h2>
+      <p>Convenience first</p>
       
        </v-col>
 
@@ -566,12 +348,13 @@
     <v-row>
    <v-col cols="3" md="3">
      <v-img
-      width="40px"
-      src="../../views/attheairport/images/joinour.svg" class="ml-2 mt-1"></v-img></v-col>
+      width="50px"
+      height="50px"
+      src="../../views/attheairport/images/joinour.svg"></v-img></v-col>
 
       <v-col cols="9" md="9">
-      <h2 class="darkgrey--text">Join our </h2>
-      <p  class="grey--text">Global Rewards Program</p>
+      <h2>Join our </h2>
+      <p>Global Rewards Program</p>
       
        </v-col>
 
@@ -583,6 +366,8 @@
    
    </v-row>
 
+    <!-- exclusive end -->
+
 
  </v-container>
     </v-card>   
@@ -590,12 +375,24 @@
 </template>
 
 <script>
+import dutyfreeBestsellerslider from '../../components/dutyfreeBestsellerslider'
+import dutyfreeNewarrival from '../../components/dutyfreeNewarrival'
+
 export default {
+
+    components: {
+        dutyfreeBestsellerslider,
+        dutyfreeNewarrival,
+      
+
+    },
+
     data() {
 
       return{
         BestsellersSlider: '',
         NewArrivalSlider:'',
+
          slider: [
         "red", 
         "green", 
@@ -611,48 +408,7 @@ export default {
         "blue-grey"
       ],
 
-       slides: [
-      {
-        title: 'TERRA PC-BUSINESS 5050S',
-          image: 'https://www.wortmann.de/content/files/images/ProductImages/medium/item100955910000.jpg',
-        price: 559
-      },
-      {
-        title: 'TERRA PC-BUSINESS 5000',
-        image: 'https://www.wortmann.de/content/files/images/ProductImages/medium/item100956890000.jpg',
-        price: 609
-      },
-      {
-        title: 'TERRA PC-Micro 6000SE SILENT GREENLINE ',
-        image: 'https://www.wortmann.de/content/files/images/ProductImages/medium/item100955650000.jpg',
-        price: 689
-      },
-      {
-        title: 'HP ENVY 5030 multifunction printer',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/81DzbjMdK8L._AC_SX569_.jpg',
-        price: 66
-      },
-      {
-        title: 'TERRA PC-BUSINESS 5050S',
-        image: 'https://www.wortmann.de/content/files/images/ProductImages/medium/item100955910000.jpg',
-        price: 559
-      },
-      {
-        title: 'TERRA PC-BUSINESS 5000',
-        image: 'https://www.wortmann.de/content/files/images/ProductImages/medium/item100956890000.jpg',
-        price: 609
-      },
-      {
-        title: 'TERRA PC-Micro 6000SE SILENT GREENLINE ',
-        image: 'https://www.wortmann.de/content/files/images/ProductImages/medium/item100955650000.jpg',
-        price: 689
-      },
-      {
-        title: 'HP ENVY 5030 multifunction printer',
-        image: 'https://images-na.ssl-images-amazon.com/images/I/81DzbjMdK8L._AC_SX569_.jpg',
-        price: 66
-      }
-    ]
+      
       }
         
     },
@@ -701,30 +457,26 @@ export default {
   background: -webkit-linear-gradient(90deg, rgba(17, 24, 39, 0.1) 0.08%, rgba(17, 24, 39, 0.3) 41.89%, rgba(17, 24, 39, 0.8) 100%); /* Chrome10+,Safari5.1+ */
   background: -o-linear-gradient(90deg, rgba(17, 24, 39, 0.1) 0.08%, rgba(17, 24, 39, 0.3) 41.89%, rgba(17, 24, 39, 0.8) 100%); /* Opera 11.10+ */
   background: -ms-linear-gradient(90deg, rgba(17, 24, 39, 0.1) 0.08%, rgba(17, 24, 39, 0.3) 41.89%, rgba(17, 24, 39, 0.8) 100%); /* IE10+ */
-  background: linear-gradient(90deg, rgba(17, 24, 39, 0.1) 0.08%, rgba(17, 24, 39, 0.3) 41.89%, rgba(17, 24, 39, 0.8) 100%); /* W3C */
- 
-   
+  background: linear-gradient(90deg, rgba(17, 24, 39, 0.1) 0.08%, rgba(17, 24, 39, 0.3) 41.89%, rgba(17, 24, 39, 0.8) 100%); /* W3C */  
 }
 
 .v-sheet.v-footer:not{ box-shadow: none;}
 .v-sheet.v-card{ box-shadow: none !important;}
 
-/* .atdariport .v-image{ border-radius: 30px !important; } */
-
-
 
 .imageBox{ position: relative; }
 
-.imageBox-txt{ position: absolute; bottom: 20px; left: 18px;}
-
-
+.imageBox-txt{ position: absolute; bottom: 45px;left: 25px;}
 
 .imageBox-txt p{
+
 font-style: normal;
-font-weight: bold;
-font-size: 20px;
-line-height: 24px;
-color: #FFFFFF; } 
+font-weight: 700;
+font-size: 36px;
+line-height: 44px;
+letter-spacing: -0.02em;
+color: #FFFFFF;
+ } 
 
 
 .imageBox-txt span{ 
@@ -743,43 +495,60 @@ color: #FFFFFF;
 .categoryBox {  border: 2px solid #E5E7EB;
 background: #FFFFFF; position: relative;}
 .categImg { position: absolute; right: 0; top: 50px;}
-.gradi-boxTwo {position:relative; display:inline-block; }
-.gradi-boxTwo::after {content:'';
-  position:absolute;
-  left:0; top:0;
-  width:100%; height:100%;
-  display:inline-block; background: -moz-linear-gradient(90deg, rgba(17, 24, 39, 0.1) 0.08%, rgba(17, 24, 39, 0.3) 41.89%, rgba(17, 24, 39, 0.8) 100%); /* FF3.6+ */
 
- background: -moz-linear-gradient(90deg, rgba(250, 251, 252, 0.927) 0.08%, rgba(235, 235, 237, 0.3) 41.89%, #f765a7cc 100%); /* FF3.6+ */
-  background: -webkit-gradient(90deg, rgba(250, 251, 252, 0.927) 0.08%, rgba(235, 235, 237, 0.3) 41.89%, #f765a7cc 100%); /* Chrome,Safari4+ */
-  background: -webkit-linear-gradient(90deg, rgba(250, 251, 252, 0.927) 0.08%, rgba(235, 235, 237, 0.3) 41.89%, #f765a7cc 100%); /* Chrome10+,Safari5.1+ */
-  background: -o-linear-gradient(90deg, rgba(250, 251, 252, 0.927) 0.08%, rgba(235, 235, 237, 0.3) 41.89%, #f765a7cc 100%); /* Opera 11.10+ */
-  background: -ms-linear-gradient(90deg, rgba(250, 251, 252, 0.927) 0.08%, rgba(235, 235, 237, 0.3) 41.89%, #f765a7cc 100%); /* IE10+ */
-  background: linear-gradient(90deg, rgba(250, 251, 252, 0.927) 0.08%, rgba(235, 235, 237, 0.3) 41.89%, #f765a7cc 100%); /* W3C */
 
-}
+.imageBox-txt-two p{position: absolute; top: 20px; left: 10px; font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 28px;
+text-align: center;
+letter-spacing: -0.02em;
+color: #374151;}
 
-.imageBox-txt-two p{ color: black;  position: absolute; top: 20px; left: 10px; font-weight: 600;}
-.amtBox {width: 80px;
-height: 40px;
-padding-top: 10px;
-
+.gradient-button {font-style: normal;
 font-weight: 700;
 font-size: 16px;
+line-height: 24px !important;
+/* identical to box height, or 150% */
+
+text-align: center;
+
+color: #FFFFFF !important;}
+
+.bestseller h2, .newarrival h2{font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 32px;
+letter-spacing: -0.02em;
+color: #374151;
+}
+
+
+.bestseller p, .newarrival p {font-style: normal;
+font-weight: 500;
+font-size: 16px;
 line-height: 24px;
-background: #E8EFFE;
-border-radius: 100px;color: #1B63F8; text-align: center;}
-
-.BestsellersSliderImgBox {
-
-background: linear-gradient(0deg, #ABDEE6, #ABDEE6), #D1D5DB;
+color: #6B7280;
 }
 
+.v-carousel{ height: 350px !important;}
 
-.NewArrivalSliderImgBox {
- background:linear-gradient(0deg, #F1DFCD, #F1DFCD), #D1D5DB;
+.bestseller, .newarrival, .featuresBlk { margin-top: 80px;}
+
+.featuresBlk h2{font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 28px;
+letter-spacing: -0.02em;
+color: #374151;
 }
 
-
+.featuresBlk p{
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 24px;
+color: #6B7280;
+}
 
 </style>
